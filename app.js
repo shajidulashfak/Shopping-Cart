b00843a1da9002acbe58bdd88c8dd665ca82ae85
shajidulashfak <shajidulashfak@gmail.com>
2021-05-03 16:20:35 +0600

    Shopping Card Done

--- app.js
+++ app.js
@@ -1,40 +1,64 @@
+function handelCountChange (product, isIncrease) {
+    const productInput = document.getElementById(product + "Count");
 
-   document.getElementById("iphoneIncrease").addEventListener('click', function() {
-    const iphoneInput = document.getElementById("iphoneCount");
-    const iPhones = parseInt(iphoneInput.value);
-    const iphoneNewCount = iPhones + 1;
-    iphoneInput.value = iphoneNewCount;
-    const iphoneTotal = iphoneNewCount * 1219;
-    document.getElementById("iphonePrice").innerText = "$" + iphoneTotal;
-   });
-   
-   document.getElementById("iphoneDecrease").addEventListener('click', function() {
-       const iphoneInput = document.getElementById("iphoneCount");
-       const iPhones = parseInt(iphoneInput.value);
-       const iphoneNewCount = iPhones - 1;
-       iphoneInput.value = iphoneNewCount;
-       const iphoneTotal = iphoneNewCount * 1219;
-       document.getElementById("iphonePrice").innerText = "$" + iphoneTotal;
-   })
-   
-   
-   
-   //For Increase Value.
-document.getElementById("caseIncrease").addEventListener('click', function(){
-    const caseInput = document.getElementById("caseCount");
-    const iphoneCase = parseInt(caseInput.value);
-    const caseNewCount = iphoneCase + 1;
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById("casePrice").innerText = "$" + caseTotal;
-});
+    // call Function
+    const productCount = getInputValue(product);
 
-//For decrease Value.
-document.getElementById("caseDecrease").addEventListener('click', function(){
-    const caseInput = document.getElementById("caseCount");
-    const iphoneCase = parseInt(caseInput.value);
-    const caseNewCount = iphoneCase - 1;
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById("casePrice").innerText = "$" + caseTotal;
-});
+    let productNewCount = productCount;
+    if (isIncrease == true) {
+        productNewCount = productCount + 1;
+    }
+   if(isIncrease == false && productCount > 0){
+    productNewCount = productCount - 1;
+    }
+
+    productInput.value = productNewCount;
+
+    let ProductPrice = 0;
+    if (product == "iPhone") {
+        ProductPrice = productNewCount * 1219;
+    }
+    if (product == "case") {
+        ProductPrice = productNewCount * 59;
+    }
+    document.getElementById(product + "Price").innerText = "$" + ProductPrice;
+
+    // function callBack
+    calculateTotal ();
+}
+// Total Amount Count 
+function calculateTotal () {
+    // for Iphone
+    // call Function
+    const phoneCount = getInputValue("iPhone")
+
+    // for Case
+    // call Function
+    const caseCount = getInputValue("case");
+
+   
+    const totalPrice = phoneCount * 1219 + caseCount * 59 ;
+    document.getElementById("total-Price").innerText = '$' + totalPrice;
+     
+     // for tax
+    const tax = Math.round(totalPrice * 0.15);
+    document.getElementById("taxValue").innerText = "$" + tax;
+
+    // For NetTotal
+    const netTotal = totalPrice + tax;
+    document.getElementById("net-total").innerText = "$" + netTotal;
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + "Count");
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
+
+// Remove Part
+const removeBtn = document.getElementById("remove");
+removeBtn.addEventListener('click', function(){
+const removeArea = document.getElementById("removeArea");
+removeArea.style.display = "none";
+})
+
